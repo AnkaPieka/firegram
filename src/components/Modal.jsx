d4a63581e88113c03ee89619d04125edd0cbfb9e
Anna Lefour <anna.lefour@laposte.net>
2021-05-04 18:41:00 +0200

    Starting carousel mode, prev button logic done

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,16 +1,45 @@
 import React from "react";
+import useFirestore from "../hooks/useFirestore";
+import { motion } from "framer-motion";
 
 const Modal = ({ selectedImg, setSelectedImg }) => {
+  const { docs } = useFirestore("images");
+  console.log(docs);
+
   const handleClick = (e) => {
     if (e.target.classList.contains("backdrop")) {
       setSelectedImg(null);
     }
   };
 
+  const prevButtonClick = (e) => {
+    console.log("si", selectedImg);
+    docs.map((doc) => {
+      for (let i = 0; i < docs.length; i++) {
+        if (doc.url === selectedImg) {
+          const prevIndex = docs.indexOf(doc)-1;
+          setSelectedImg(docs[prevIndex].url);
+        }
+      }
+    });
+  };
+
   return (
-    <div className="backdrop" onClick={handleClick}>
-      <img src={selectedImg} alt="one pic" />
-    </div>
+    <motion.div
+      className="backdrop"
+      onClick={handleClick}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+    >
+      <button onClick={prevButtonClick}>Previous</button>
+      <motion.img
+        src={selectedImg}
+        alt="one pic"
+        initial={{ y: "-100vh" }}
+        animate={{ y: 0 }}
+      />
+      <button onClick="nextButtonClick">Next</button>
+    </motion.div>
   );
 };
 
